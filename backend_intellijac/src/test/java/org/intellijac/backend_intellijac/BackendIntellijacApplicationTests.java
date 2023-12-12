a0c81de67a61f500c9bd694a3310d0283070ab6e
jpbarragan <jpbarragann@gmail.com>
2023-12-11 21:12:49 -0500

    "Created full deck apps"

---
+++ backend_intellijac/src/test/java/org/intellijac/backend_intellijac/BackendIntellijacApplicationTests.java
@@ -0,0 +1,13 @@
+package org.intellijac.backend_intellijac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendIntellijacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
