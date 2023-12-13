6072cc18e6b4850b43309ab71e36e467e5799aae
jpbarragan <jpbarragann@gmail.com>
2023-12-13 16:37:01 -0500

    "Created Flashcards.js file in the frontend (2)"

---
+++ frontend_intellijac/intellijac-frontend/src/Flashcards.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Flashcards () {
+    return (
+        <div>
+            <h1>Create your own math flashcards!</h1>
+        </div>
+    );
+}
+
+export default Flashcards;
