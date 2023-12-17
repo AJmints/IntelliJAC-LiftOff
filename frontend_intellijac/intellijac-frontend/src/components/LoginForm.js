cfc067831d0a097ef8ecd639205f94519b821629
91charu <134026214+91charu@users.noreply.github.com>
2023-12-16 19:54:08 -0600

    updated login and register form

--- frontend_intellijac/intellijac-frontend/src/components/LoginForm.js
+++ frontend_intellijac/intellijac-frontend/src/components/LoginForm.js
@@ -1,3 +1,154 @@
+//import React from 'react';
+//import classNames from 'classnames';
+//import './app.css';
+//
+//export default class LoginForm extends React.Component {
+//  constructor(props) {
+//    super(props);
+//    this.state = {
+//      firstName: '',
+//      lastName: '',
+//      username: '',
+//      password: '',
+//      isRegistering: false,
+//      errors: {},
+//    };
+//  }
+//
+//  validateForm = () => {
+//    const { username, password, firstName, lastName } = this.state;
+//    const errors = {};
+//
+//    // Validate username
+//    if (!username.trim()) {
+//      errors.username = 'Username is required';
+//    }
+//
+//    // Validate password
+//    if (!password.trim()) {
+//      errors.password = 'Password is required';
+//    } else if (password.length < 3 || password.length > 40) {
+//      errors.password = 'Password must be between 3 and 40 characters';
+//    }
+//
+//    // Validate first name
+//    if (this.state.isRegistering && !firstName.trim()) {
+//      errors.firstName = 'First name is required';
+//    }
+//
+//    // Validate last name
+//    if (this.state.isRegistering && !lastName.trim()) {
+//      errors.lastName = 'Last name is required';
+//    }
+//
+//    this.setState({ errors });
+//    return Object.keys(errors).length === 0;
+//  };
+//
+//  onChangeHandler = (event) => {
+//    let name = event.target.name;
+//    let value = event.target.value;
+//    this.setState({ [name]: value });
+//  };
+//
+//  onSubmit = (e) => {
+//    e.preventDefault();
+//
+//    if (!this.validateForm()) {
+//      return;
+//    }
+//
+//    const { isRegistering, firstName, lastName, username, password } = this.state;
+//
+//    if (isRegistering) {
+//      this.props.onRegister(firstName, lastName, username, password);
+//    } else {
+//      this.props.onLogin(username, password);
+//    }
+//  };
+//
+//  render() {
+//    const { isRegistering, errors } = this.state;
+//
+//    return (
+//      <div className="login-form-container">
+//        <div className="button-container">
+//          <button
+//            onClick={() => this.setState({ isRegistering: false })}
+//            className={classNames("button", { active: !isRegistering })}
+//          >
+//            Login
+//          </button>
+//          <button
+//            onClick={() => this.setState({ isRegistering: true })}
+//            className={classNames("button", { active: isRegistering })}
+//          >
+//            Register
+//          </button>
+//        </div>
+//
+//        <form onSubmit={this.onSubmit}>
+//          {isRegistering && (
+//            <>
+//              <label className="form-label">
+//                First Name:
+//                <input
+//                  className="form-input"
+//                  type="text"
+//                  name="firstName"
+//                  value={this.state.firstName}
+//                  onChange={this.onChangeHandler}
+//                />
+//                {errors.firstName && <div className="error-message">{errors.firstName}</div>}
+//              </label>
+//              <label className="form-label">
+//                Last Name:
+//                <input
+//                  className="form-input"
+//                  type="text"
+//                  name="lastName"
+//                  value={this.state.lastName}
+//                  onChange={this.onChangeHandler}
+//                />
+//                {errors.lastName && <div className="error-message">{errors.lastName}</div>}
+//              </label>
+//            </>
+//          )}
+//
+//          <label className="form-label">
+//            Username:
+//            <input
+//              className="form-input"
+//              type="text"
+//              name="username"
+//              value={this.state.username}
+//              onChange={this.onChangeHandler}
+//            />
+//            {errors.username && <div className="error-message">{errors.username}</div>}
+//          </label>
+//          <label className="form-label">
+//            Password:
+//            <input
+//              className="form-input"
+//              type="password"
+//              name="password"
+//              value={this.state.password}
+//              onChange={this.onChangeHandler}
+//            />
+//            {errors.password && <div className="error-message">{errors.password}</div>}
+//          </label>
+//          <div className="centered-button-container">
+//            <button className="centered-button" type="submit">
+//              Submit
+//            </button>
+//          </div>
+//        </form>
+//      </div>
+//    );
+//  }
+//}
+
+
 import React from 'react';
 import classNames from 'classnames';
 import './app.css';
@@ -11,39 +162,8 @@ export default class LoginForm extends React.Component {
       username: '',
       password: '',
       isRegistering: false,
-      errors: {},
     };
   }
-
-  validateForm = () => {
-    const { username, password, firstName, lastName } = this.state;
-    const errors = {};
-
-    // Validate username
-    if (!username.trim()) {
-      errors.username = 'Username is required';
-    }
-
-    // Validate password
-    if (!password.trim()) {
-      errors.password = 'Password is required';
-    } else if (password.length < 3 || password.length > 40) {
-      errors.password = 'Password must be between 3 and 40 characters';
-    }
-
-    // Validate first name
-    if (this.state.isRegistering && !firstName.trim()) {
-      errors.firstName = 'First name is required';
-    }
-
-    // Validate last name
-    if (this.state.isRegistering && !lastName.trim()) {
-      errors.lastName = 'Last name is required';
-    }
-
-    this.setState({ errors });
-    return Object.keys(errors).length === 0;
-  };
 
   onChangeHandler = (event) => {
     let name = event.target.name;
@@ -53,10 +173,6 @@ export default class LoginForm extends React.Component {
 
   onSubmit = (e) => {
     e.preventDefault();
-
-    if (!this.validateForm()) {
-      return;
-    }
 
     const { isRegistering, firstName, lastName, username, password } = this.state;
 
@@ -68,7 +184,7 @@ export default class LoginForm extends React.Component {
   };
 
   render() {
-    const { isRegistering, errors } = this.state;
+    const { isRegistering } = this.state;
 
     return (
       <div className="login-form-container">
@@ -99,7 +215,6 @@ export default class LoginForm extends React.Component {
                   value={this.state.firstName}
                   onChange={this.onChangeHandler}
                 />
-                {errors.firstName && <div className="error-message">{errors.firstName}</div>}
               </label>
               <label className="form-label">
                 Last Name:
@@ -110,7 +225,6 @@ export default class LoginForm extends React.Component {
                   value={this.state.lastName}
                   onChange={this.onChangeHandler}
                 />
-                {errors.lastName && <div className="error-message">{errors.lastName}</div>}
               </label>
             </>
           )}
@@ -124,7 +238,6 @@ export default class LoginForm extends React.Component {
               value={this.state.username}
               onChange={this.onChangeHandler}
             />
-            {errors.username && <div className="error-message">{errors.username}</div>}
           </label>
           <label className="form-label">
             Password:
@@ -135,7 +248,6 @@ export default class LoginForm extends React.Component {
               value={this.state.password}
               onChange={this.onChangeHandler}
             />
-            {errors.password && <div className="error-message">{errors.password}</div>}
           </label>
           <div className="centered-button-container">
             <button className="centered-button" type="submit">
