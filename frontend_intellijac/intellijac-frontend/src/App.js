e07b9a574ed71e390ab4e913007b750726baf575
jpbarragan <jpbarragann@gmail.com>
2024-01-01 06:36:31 -0500

    Created page with searchable list of resources.

--- frontend_intellijac/intellijac-frontend/src/App.js
+++ frontend_intellijac/intellijac-frontend/src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import './components/Rating.js';
 import Ratings from './components/Rating.js';
 import Flashcards from './components/Flashcards';
+import Resources from './components/Resources.js';
 
 function App() {
   return (
     <div className="App">
-      <Flashcards/>
+      <Resources/>
     </div>
   );
 }
---
+++ frontend_intellijac/intellijac-frontend/src/components/resourceslist.js
@@ -0,0 +1,51 @@
+export const ResourcesList = [
+    {
+        "id": 1,
+        "name": "Khan Academy", 
+        "url": "https://www.khanacademy.org/math"
+    },
+    {
+        "id": 2,
+        "name": "Learn Math Tutorials on Youtube", 
+        "url": "https://www.youtube.com/user/learnmathtutorials"
+    },
+    {
+        "id": 3,
+        "name": "Math Tutorials Library", 
+        "url": "https://www.tutorialspoint.com/maths_tutorials.htm"
+    },
+    {
+        "id": 4,
+        "name": "Pauls Online Math Notes", 
+        "url": "https://tutorial.math.lamar.edu/"
+    },
+    {
+        "id": 5,
+        "name": "Math Antics", 
+        "url": "https://mathantics.com/"
+    },
+    {
+        "id": 6,
+        "name": "Math Resources at MIT Blossoms", 
+        "url": "https://blossoms.mit.edu/resources/math_resources"
+    }, {
+        "id": 7,
+        "name": "Educational Math Toys", 
+        "url": "https://www.learningresources.com/shop/subject/math"
+    },
+    {
+        "id": 8,
+        "name": "Mathigon – The Mathematical Playground", 
+        "url": "https://mathigon.org/"
+    },
+    {
+        "id": 9,
+        "name": "Illustrative Mathematics | K-12 Math", 
+        "url": "https://illustrativemathematics.org/"
+    },
+    {
+        "id": 10,
+        "name": "The Science of Math", 
+        "url": "https://www.thescienceofmath.com/"
+    },
+]
