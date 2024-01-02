49a79c1fa52dada4acfdca4622eed12185c4c8ca
jpbarragan <jpbarragann@gmail.com>
2024-01-01 20:55:26 -0500

    Added router links and blank edit flashcards page.

--- frontend_intellijac/intellijac-frontend/src/App.js
+++ frontend_intellijac/intellijac-frontend/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 import './components/Rating.js';
 import Ratings from './components/Rating.js';
 import Flashcards from './components/Flashcards';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import EditFlashcards from './components/EditFlashcards.js';
 
 function App() {
   return (
     <div className="App">
-      <Flashcards/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Flashcards/>}/>
+          <Route exact path="/editflashcard" element={<EditFlashcards/>}/>
+        </Routes>
+      </Router>
+      
     </div>
   );
 }
