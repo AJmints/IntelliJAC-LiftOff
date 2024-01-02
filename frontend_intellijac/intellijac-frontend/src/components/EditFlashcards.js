bd80a10f98a982f25c01e334e174e4ec1d5a655f
jpbarragan <jpbarragann@gmail.com>
2024-01-01 20:56:43 -0500

    Added router links and blank edit flashcards page - complete.

---
+++ frontend_intellijac/intellijac-frontend/src/components/EditFlashcards.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function EditFlashcards() {
+
+    return (
+        <div>
+            Edit Flashcards page
+        </div>
+    )
+
+}
+
+export default EditFlashcards;
