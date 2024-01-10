a9b5e869a9d4c20984f0e4af1d66d977bc1ea16b
Jialing Liu <jialingl.liu@gmail.com>
2024-01-10 12:55:12 -0500

    Created user comment class, controller and repository to save user comments in database.

---
+++ backend_intellijac/src/main/java/com/intellijac/backend_intellijac/models/UserComment.java
@@ -0,0 +1,59 @@
+package com.intellijac.backend_intellijac.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
+public class UserComment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "user_comment")
+    private String comment;
+
+    public UserComment(){}
+
+    public UserComment(String comment) {
+        super();
+        this.comment = comment;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+
+    @Override
+    public String toString() {
+        return "UserComment{" +
+                "comment='" + comment + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserComment that = (UserComment) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
